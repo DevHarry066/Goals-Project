9711fdb3f740ae0ce3fafef7f16b26f194cd31af
Hariom <82692072+DevHarry066@users.noreply.github.com>
2023-11-29 09:51:15 -0800

    Frontend Register and Login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Dashboard from './pages/dashboard';
+import Login from './pages/login';
+import Register from './pages/register';
 
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
+    <>
+    <Router>
+    <div className='container'>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={ <Dashboard/> }></Route>
+        <Route path='/register' element={ <Register/> }></Route>
+        <Route path='/login' element={ <Login/> }></Route>
+      </Routes>
     </div>
+    </Router>
+    </>
   );
 }
 
-export default App;
+export default App; 
