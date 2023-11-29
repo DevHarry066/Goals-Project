64a82eed61a511b7222ee3c20ed5f9dd6d90405f
Hariom <82692072+DevHarry066@users.noreply.github.com>
2023-11-29 10:18:33 -0800

    Added redux authSlice file

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,28 @@
+import { createSlide, createAsyncTrunk } from 'reduxjs/toolkit';
+
+const user = JSON.parse(localStorage.getItem('user'));
+
+const intialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    intialState,
+    reducers: {
+        reset: (state) => {
+            state.isLoading =  false
+            state.isError = false
+            state.isSuccess = false
+            state.message = ''
+        },
+    },
+    extraReducers: () => {},
+})
+
+export const { reset } = authSlice.actions
+export default authSlice.reducer
